b3687bf9277a453355398f56846fdacc28980470
vishavbangotra <44341523+vishavbangotra@users.noreply.github.com>
2020-06-16 23:27:02 +0530

    Completed homepage

--- static/script/Index.js
+++ static/script/Index.js
@@ -16,6 +16,15 @@ $('.linkedin-box').mouseleave(function(){
     $('#linkedin-logo').css("filter", 'none');
     $('.linkedin-box').css("box-shadow", '-6px -6px 26px rgba(255, 255, 255 ,0.83), 6px 6px 16px rgba(217, 210, 200, 0.51)');
 })
+$('.resume-box').mouseenter(function(){
+  $('#cv-logo').css("filter", 'brightness(0) saturate(100%)invert(73%) sepia(59%) saturate(5851%) hue-rotate(360deg) brightness(107%) contrast(106%)')
+  $('.resume-box').css("box-shadow", 'inset -6px -6px 26px rgba(255, 255, 255 ,0.83), inset 6px 6px 16px rgba(217, 210, 200, 0.51)');
+})
+$('.resume-box').mouseleave(function(){
+  $('#cv-logo').css("filter", 'none');
+  $('.resume-box').css("box-shadow", '-6px -6px 26px rgba(255, 255, 255 ,0.83), 6px 6px 16px rgba(217, 210, 200, 0.51)');
+})
+
 
 // box-shadow: -6px -6px 26px rgba(255, 255, 255 ,0.83), 6px 6px 16px rgba(217, 210, 200, 0.51);
 
